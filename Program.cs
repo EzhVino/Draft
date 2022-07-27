c057698db697fe14eb481d1749a02030bc735566
Ezh+Wine <valvic90@gmail.com>
2022-07-27 23:47:01 +0300

    new commit 2

--- Program.cs
+++ Program.cs
@@ -1,70 +1,9 @@
-﻿
-
-// string[] CreateArray(int size)
-// {
-//     string[] array = new string[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         Console.Write($"Input element {i + 1} of the massive: ");
-//         array[i] = Convert.ToString(Console.ReadLine());
-//     }
-//     return array;
-// }
-
-// void ShowArray(string[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i] + " ");
-//     }
-// }
-
-// int CountElements(string[] massive, int elementSize)
-// {
-//     int count = 0;
-//     for (int i = 0; i < massive.Length; i++)
-//     {
-//         if (massive[i].Length <= elementSize)
-//             count++;
-//     }
-//     return count;
-// }
-
-// string [] ModifyMassive(string[] massive, int elementSize)
-// {
-//     int newSize = CountElements(massive, elementSize);
-//     string[] newMassive = new string[newSize];
-//     int i = 0;
-//     int j = 0;
-
-//     while (j < newSize)
-//     {
-//         while (i < massive.Length)
-//         {
-//             if (massive[i].Length <= elementSize)
-//             {
-//                 newMassive[j] = massive[i];
-//                 j++;
-//             }
-//             i++;
-//         }
-//     }
-//     return newMassive;
-// }
-
-// Console.Clear();
-// string [] myArray = CreateArray(4);
-
-// ShowArray(ModifyMassive(myArray, 3));
-
-
-
-string[] CreateArray(int size)
+﻿string[] CreateArray(int size)
 {
     string[] array = new string[size];
     for (int i = 0; i < size; i++)
     {
-        Console.Write($"Input element {i + 1} of the massive: ");
+        Console.Write($"Input element {i + 1}: ");
         array[i] = Convert.ToString(Console.ReadLine());
     }
     return array;
@@ -74,7 +13,7 @@ void ShowArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write(array[i] + " | ");
     }
 }
 
@@ -118,9 +57,8 @@ void ModifyMassive(string[] massive, int elementSize)
 }
 
 Console.Clear();
-string [] myArray = CreateArray(4);
-
+Console.WriteLine("Enter size of the massive: ");
+int arraySize = int.Parse(Console.ReadLine());
+string [] myArray = CreateArray(arraySize);
+Console.WriteLine();
 ModifyMassive(myArray, 3);
-
-
-
